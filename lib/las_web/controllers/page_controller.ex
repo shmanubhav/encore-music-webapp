b1bd08092bbcb977a659b708a933432033acc459
sbssai123 <sbs.sai@gmail.com>
2018-10-30 14:33:19 -0400

    inital pass at spotify authentication

--- lib/las_web/controllers/page_controller.ex
+++ lib/las_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule LasWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def login(conn, _params) do
+    render(conn, "index.html")
+  end
 end
--- lib/las_web/router.ex
+++ lib/las_web/router.ex
@@ -17,6 +17,7 @@ defmodule LasWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/login", PageController, :login
   end
 
   # Other scopes may use custom stacks.
