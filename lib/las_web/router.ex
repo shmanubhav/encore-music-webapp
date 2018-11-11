45722c42af6c8808912dc30e656534c9e22de9f8
Lindsay Hauser <>
2018-11-11 18:17:26 -0500

    router changes for auth code

--- lib/las_web/router.ex
+++ lib/las_web/router.ex
@@ -1,3 +1,5 @@
+# Used Reference: https://github.com/scrogson/oauth2_example/blob/master/web/router.ex
+
 defmodule LasWeb.Router do
   use LasWeb, :router
 
@@ -7,6 +9,8 @@ defmodule LasWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :assign_current_user
+
   end
 
   pipeline :api do
@@ -18,6 +22,21 @@ defmodule LasWeb.Router do
 
     get "/", PageController, :index
     get "/login", PageController, :login
+  end
+
+  scope "/auth", OAuth2Example do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :index
+    get "/:provider/callback", AuthController, :callback
+    delete "/logout", AuthController, :delete
+  end
+
+  # Fetch the current user from the session and add it to `conn.assigns`. This
+  # will allow you to have access to the current user in your views with
+  # `@current_user`.
+  defp assign_current_user(conn, _) do
+    assign(conn, :current_user, get_session(conn, :current_user))
   end
 
   # Other scopes may use custom stacks.
