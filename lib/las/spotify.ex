925dae4be1a4b870a545bf116928fd72eb810896
Lindsay Hauser <>
2018-11-14 20:24:03 -0500

    using httpoison to get a token back for user

--- lib/las/spotify.ex
+++ lib/las/spotify.ex
@@ -19,32 +19,13 @@ defmodule Spotify do
     OAuth2.Client.authorize_url!(client(), scope: "playlist-modify-public user-modify-playback-state user-read-recently-played")
   end
 
-  @url "https://accounts.spotify.com/api/token"
-
-
   # you can pass options to the underlying http library via `opts` parameter
-  def get_token!(params \\ [], headers \\ [], opts \\ []) do
+  def get_token!(params) do # \\ [], headers \\ [], opts \\ []) do
     IO.puts("In spotify.ex get token")
-    #a = HTTPoison.post(@url, idk(params), headerFormat())
-    #IO.inspect(a)
-    #OAuth2.Client.get_token!(client(), params, headers, opts)
-
-    # {:form, [{K, V}, ...]}
-    #
-    # body = {:form, [{code: params},
-    # {grant_type: "authorization_code"},
-    # {redirect_uri: client().redirect_uri}]}
-
-  #
-  # body = {:form, {[code: params],
-  # [grant_type: "authorization_code"],
-  # [redirect_uri: client().redirect_uri]}}
-
-    #req_body = URI.encode_query(post_body(params))
 
     response = HTTPoison.post(
-      @url,
-      idk(params),
+      client().token_url,
+      idk3(params),
       headerFormat()
     )
     IO.puts("GOT HERE _+_+_+_+_+_+_+_+_+_+_+_+_+_++_+_+_+_+__+_+_+_+_+_+_+_+")
@@ -55,9 +36,7 @@ defmodule Spotify do
 
 
 # FROM THE API DOCS
-
 # https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
-
 # REQUEST BODY PARAMETER	VALUE
 # grant_type	Required.
 # As defined in the OAuth 2.0 specification, this field must contain the value "authorization_code".
@@ -70,14 +49,8 @@ defmodule Spotify do
 # Base 64 encoded string that contains the client ID and client secret key. The field must have the format: Authorization: Basic *<base64 encoded client_id:client_secret>*
 # *** An alternative way to send the client id and secret is as request parameters (client_id and client_secret) in the POST body, instead of sending them base64-encoded in the header.
 
-# On success, the response from the Spotify Accounts service has the status code 200 OK in the response header, and the following JSON data in the response body:
-
-  # I assume these parameters will get sent in as the request body parametesrs.
-  # In this case,
-
   def idk(params) do
     "grant_type=authorization_code&code=#{params}&redirect_uri=#{client().redirect_uri}" #|> Poison.encode!
-
   end
 
   def post_body(params) do
@@ -88,6 +61,18 @@ defmodule Spotify do
     # "client_id"=> client().client_id,
     # "client_secret" => client().client_secret
   } #|> URI.encode_query
+  end
+
+  def idk3(params) do
+    body =
+     %{
+       grant_type: "authorization_code",
+       code: params,
+       redirect_uri: client().redirect_uri,
+       client_id: client().client_id,
+       client_secret: client().client_secret
+     } |> URI.encode_query
+     body
   end
 
   def idk2(params) do
@@ -130,7 +115,7 @@ defmodule Spotify do
 
   def headerFormat do
     [
-      {"Authorization", "Basic #{encoded_credentials()}"},
+    #  {"Authorization", "Basic #{encoded_credentials()}"},
       {"Content-Type", "application/x-www-form-urlencoded"}
     ]
   end
@@ -148,7 +133,10 @@ defmodule Spotify do
   # end
 
   # If we want to send client id and secret in 64 bit encoded in the header params
-  def encoded_credentials, do: :base64.encode("#{client().client_id}:#{client().client_secret}")
+  def encoded_credentials do
+    Base.encode64("#{client().client_id}:#{client().client_secret}")
+  end
+    #base64.encode("#{client().client_id}:#{client().client_secret}")
 
   # Strategy Callbacks
 
