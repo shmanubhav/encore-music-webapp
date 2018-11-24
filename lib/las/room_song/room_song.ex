063896952fb6e328d32fd7fccd0102197579dd70
sbssai123 <sbs.sai@gmail.com>
2018-11-23 21:43:47 -0500

    added room song

---
+++ lib/las/room_song/room_song.ex
@@ -0,0 +1,17 @@
+defmodule Las.RoomSongs.RoomSongs do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "room_song" do
+    belongs_to :song, Las.Songs.Song
+    belongs_to :room, Las.Rooms.Room
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(roomsong, attrs) do
+    roomsong
+    |> cast(attrs, [:song_id, :room_id])
+  end
+end
